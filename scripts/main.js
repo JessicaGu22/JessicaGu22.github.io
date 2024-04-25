5acc35756ec3b8f191e4fc2c4dde2b7759ee5e25
JessicaGu22 <144717208+JessicaGu22@users.noreply.github.com>
2024-04-25 22:24:35 +0000

    add tags

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+      console.log("Task 1:");
+      for (let i = 1; i <= 10; i++) {
+        console.log(i * i);
+      }
+
+      console.log("Task 2:");
+      let countdown = 5; 
+      while (countdown > 0) {
+        console.log(countdown);
+        countdown--;
+     }
+     console.log("Task 3:");
+     for (let i = 2; i <= 50; i += 2) {
+        console.log(i);
+     }
+     console.log("Task 4:");
+     let sum = 0;
+     for (let i = 1; i <= 100; i++) {
+       sum += i;
+     }
+    </body>
+</html>
+
