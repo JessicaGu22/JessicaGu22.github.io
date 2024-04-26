8229ea810c3fdbd2910ee127299107d88911731f
JessicaGu22 <144717208+JessicaGu22@users.noreply.github.com>
2024-04-26 04:27:16 +0000

    assignment JS: For-loops and While-loops add tags

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<html>
-<head>
-</head>
-<body>
-    console.log("Task 1:");
-    for (let i = 1; i <= 10; i++) {
-      console.log(i * i);
-    }
 
+console.log("Task 1:");
+for (var i = 0; i < 11; i++) {
+    var product = i * i;
+    console.log(i + " squared  = " + product);
+}
 
-    console.log("Task 2:");
-    let countdown = 5; 
-    while (countdown > 0) {
-      console.log(countdown);
-    }
-    console.log("Blastoff!");
+console.log("Task 2:");
+let countdown = 5;
+while (countdown > 0) {
+    console.log(countdown);
+    countdown--;
+}
+console.log("Blastoff!");
 
+console.log("Task 3:");
+for (let i = 2; i <= 50; i += 2) {
+    console.log(i);
+}
 
-    console.log("Task 3:");
-    for (let i = 2; i <= 50; i += 2) {
-      console.log(i);
-    }
-
-
-    console.log("Task 4:");
-    let sum = 0;
-    for (let i = 1; i <= 100; i++) {
-        sum += i;
-    }
-
-    console.log("Task 5:");
-    for (let i = 1; i <= 10; i++) {
-        let factorial = 1;
-        for (let j = 1; j <= i; j++) {
-            factorial *= j;
-        }
-        console.log(i + "! =", factorial);
-    }
-
+console.log("Task 4:");
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+}
 console.log(sum);
-</body>
-</html>
 
+console.log("Task 5:");
+for (let i = 1; i <= 10; i++) {
+    let factorial = 1;
+    for (let j = 1; j <= i; j++) {
+        factorial *= j;
+    }
+    console.log(i + "! =", factorial);
+}
