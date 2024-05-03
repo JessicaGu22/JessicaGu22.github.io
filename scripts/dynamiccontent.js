209b8e98d1b3559cf38712380db17f1e03a2c28c
JessicaGu22 <144717208+JessicaGu22@users.noreply.github.com>
2024-05-03 19:40:09 +0000

    add tags

---
+++ scripts/dynamiccontent.js
@@ -0,0 +1,19 @@
+let arr = ["#c0c0c0", "#800080	", "#ffff00	", "#0000ff	", "#5f9ea0	", "#008b8b	", "#ff1493	"];
+let ul =  document.createElement('ul');
+let li = document.createElement('li');
+li.textContent = color;
+ul.appendChild(listItem);
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.createElement('ul');
