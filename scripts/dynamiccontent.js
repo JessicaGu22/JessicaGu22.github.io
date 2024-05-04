0efd7bbac5254a3c0450b4b5adbd0fbce6db71a4
JessicaGu22 <144717208+JessicaGu22@users.noreply.github.com>
2024-05-04 20:16:38 +0000

    add tags

--- scripts/dynamiccontent.js
+++ scripts/dynamiccontent.js
@@ -1,23 +1,3 @@
-
-
-const li = document.createElement('li');
-li.textContent = color;
-ul.appendChild(listItem);
-populateColorList();
-
-
-const arr = ["#c0c0c0", "#800080", "#ffff00", "#0000ff", "#5f9ea0", "#008b8b", "#ff1493"];
-const ul = document.createElement('ul');
-
-arr.forEach(color => {
-  const li = document.createElement('li'); 
-  const innerTextOutput = document.getElementById("arr");
-  innerTextOutput.value = arr.innerText;
-  ul.appendChild(li); 
-});
-
-document.body.appendChild(ul); 
-
 const colors = [
   "red",
   "blue",
