5649d893c8f988833f847e71c4bd7b698e7f5740
JessicaGu22 <144717208+JessicaGu22@users.noreply.github.com>
2024-06-05 18:23:49 +0000

    add

--- scripts/work.js
+++
@@ -1,7 +0,0 @@
-let currentIndex = 0;
-
-function moveSlides(n) {
-  const slides = document.querySelectorAll('.slides img');
-  currentIndex = (currentIndex + n + slides.length) % slides.length;
-  document.querySelector('.slides').style.transform = `translateX(-${currentIndex * 100}%)`;
-}
