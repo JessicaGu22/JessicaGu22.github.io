463e977325bb9460b2deb5d3af611f4aa9331837
JessicaGu22 <144717208+JessicaGu22@users.noreply.github.com>
2024-06-06 06:38:57 +0000

    edit

--- scripts/work.js
+++ scripts/work.js
@@ -1,3 +1,20 @@
-function displayProject(project) {
-    alert("You clicked on the " + project + " project!");
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var gameCard = document.getElementById("game-card");
+    var artCard = document.getElementById("art-card");
+
+    gameCard.addEventListener("mouseenter", function() {
+        gameCard.style.backgroundColor = "lavender";
+    });
+
+    gameCard.addEventListener("mouseleave", function() {
+        gameCard.style.backgroundColor = "white";
+    });
+
+    artCard.addEventListener("mouseenter", function() {
+        artCard.style.backgroundColor = "lavender";
+    });
+
+    artCard.addEventListener("mouseleave", function() {
+        artCard.style.backgroundColor = "white";
+    });
+});
